9e09711d5a4cb5345696336d28aded0c45064885
SHIVENDRA8004 <jat13632@gmail.com>
2023-06-09 00:39:24 +0530

    Responsive completed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,32 +8,33 @@ const Login = () => {
         alert("Form submitted");
     };
     return (
-        <div className="bg-drk container-fluid d-flex justify-content-center login">
-            <div className="row h-100 w-100 align-items-center">
-                <div className="col h-75 w-100 p-0">
-                    <div className="h-100 w-100">
-                        <div className="container-login h-100 w-100">
-                            <div className="text-center p-2 w-100 h-25">
-                                <h1>Verify Using Face Recognition</h1>
-                                <p className="m-0 fs-4">Align Your Face Inside Camera</p>
-                            </div>
-                            <div className="d-block d-sm-flex w-100 h-75">
-                                <div className="w-sm-50 w-100 h-100">
-                                    <WebCam />
+        <div className="bg-drk container-fluid d-flex justify-content-center align-items-center login">
+            <div className="row w-75 h-75 h-100">
+                <div className="col container-login container-fluid">
+                    <div className="row ">
+                        <div className="col text-center p-2">
+                            <h1>Verify Using Face Recognition</h1>
+                            <p className="m-0 fs-4">Align Your Face Inside Camera</p>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <div className="col col-sm-6">
+                            <WebCam />
+                        </div>
+                        <div className="col col-sm-6">
+                            <form className="form p-2 m-auto">
+                                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                                <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+                                <input type="password" placeholder="Password" onChange={(e) => setPass(e.target.value)} />
+                                <div className="d-flex justify-content-between align-items-center mt-5">
+                                    <button>
+                                        <a href="/"> Cancel</a>
+                                    </button>
+                                    <button type="submit" id="login-button" onClick={(e) => submitForm(e)}>
+                                        Submit
+                                    </button>
                                 </div>
-                                <div className="w-sm-50 w-100 h-100 d-flex align-items-center justify-content-center text-center">
-                                    <div className="h-100">
-                                        <form className="form p-1 position-relative m-auto z-2">
-                                            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                                            <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
-                                            <input type="password" placeholder="Password" onChange={(e) => setPass(e.target.value)} />
-                                            <button type="submit" className="mt-5 " id="login-button" onClick={(e) => submitForm(e)}>
-                                                Submit
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
