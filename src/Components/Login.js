04d5d868bb2066bfc161e8e338e499de6bf390ce
SHIVENDRA8004 <jat13632@gmail.com>
2023-06-08 12:20:51 +0530

    Added basic image capture login form , re creating according to requirements

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import WebCam from "./WebCam";
 const Login = () => {
-    return <div>Login</div>;
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const submitForm = () => {
+        alert("Form submitted");
+    };
+    return (
+        <div className="bg-dark">
+            <div className="home-container">
+                <div className="container">
+                    <div className="text">
+                        <h1>Fill up this form!</h1>
+                        <form className="form">
+                            <WebCam />
+                            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                            <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+                            <button type="submit" id="login-button" onClick={(e) => submitForm(e)}>
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
---
+++ src/Components/WebCam.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Webcam from "react-webcam";
+const WebcamComponent = () => <Webcam />;
+const videoConstraints = {
+    width: 220,
+    height: 200,
+    facingMode: "user",
+};
+const WebCam = () => {
+    const [image, setImage] = useState("");
+    const webcamRef = React.useRef(null);
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImage(imageSrc);
+    });
+
+    return (
+        <div classname="bg-dark">
+            <div className="webcam-container">
+                <div className="webcam-img">
+                    {image === "" ? (
+                        <Webcam audio={false} height={200} ref={webcamRef} screenshotFormat="image/jpeg" width={220} videoConstraints={videoConstraints} />
+                    ) : (
+                        <img src={image} alt="imge" />
+                    )}
+                </div>
+                <div>
+                    {image !== "" ? (
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setImage("");
+                            }}
+                            className="webcam-btn"
+                        >
+                            Retake Image
+                        </button>
+                    ) : (
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                capture();
+                            }}
+                            className="webcam-btn"
+                        >
+                            Capture
+                        </button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WebCam;
